c3ccbbbc1b83c62e97c2a2ab2a4f30b3506a3472
gleb-code <gleb.stepanov.2010@mail.ru>
2025-04-07 22:58:07 +0300

    lesson9

--- test28/src/main/java/org/example/Main.java
+++ test28/src/main/java/org/example/Main.java
@@ -1,194 +1,65 @@
 package org.example;
 
+
+// Определяем исключения
+class MyArraySizeException extends Exception {
+    public MyArraySizeException(String message) {
+        super(message);
+    }
+}
+
+class MyArrayDataException extends Exception {
+    public MyArrayDataException(String message) {
+        super(message);
+    }
+}
+
+// Основной класс
 public class Main {
     public static void main(String[] args) {
-        Bowl bowl = new Bowl(20); // Создаем миску с 20 единицами еды
-
-        Cat[] cats = {
-                new Cat("Мурзик"),
-                new Cat("Барсик"),
-                new Cat("Снежок")
+        String[][] array = {
+                {"1", "2", "3", "3"},
+                {"5", "6", "7", "8"},
+                {"9", "10", "11", "1"},
+                {"13", "14", "15", "16"}
         };
 
-        Dog dog = new Dog("Шарик");
-
-        // Коты пытаются покушать из миски
-        for (Cat cat : cats) {
-            cat.eat(bowl, 10); // Каждый кот пытается покушать 10 еды
-            cat.run(150); // Каждый кот пробегает 150 метров
-            cat.swim(1);
+        try {
+            int sum = sumArray(array);
+            System.out.println("Сумма элементов массива: " + sum);
+            System.out.println(array[5][5]);
+        } catch (MyArraySizeException e) {
+            System.out.println("Ошибка размера массива: " + e.getMessage());
+        } catch (MyArrayDataException e) {
+            System.out.println("Ошибка данных в массиве: " + e.getMessage());
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Ошибка: выход за пределы массива.");
         }
 
-        // Вывод информации о сытости котов
-        for (Cat cat : cats) {
-            System.out.println(cat.name + " сытость: " + (cat.isFull() ? "Сыт" : "Голоден"));
-        }
 
-        // Добавляем еду в миску
-        bowl.addFood(15); // Добавляем 15 единиц еды
-
-        // Коты пытаются покушать снова
-        for (Cat cat : cats) {
-            cat.eat(bowl, 10); // Каждый кот снова пытается покушать 10 еды
-            cat.run(150); // Каждый кот пробегает 150 метров
-            cat.swim(2);
-        }
-
-        // Вывод информации о сытости котов
-        for (Cat cat : cats) {
-            System.out.println(cat.name + " сытость: " + (cat.isFull() ? "Сыт" : "Голоден"));
-        }
-
-        // Собака пробегает расстояние
-        dog.run(300); // Собака пробегает 300 метров
-        dog.swim(5); // Собака плывет 5 метров
-
-        // Вывод общего количества животных
-        System.out.println("Всего животных: " + Animal.getTotalCount());
-        System.out.println("Всего собак: " + Dog.getDogCount());
-        System.out.println("Всего котов: " + Cat.getCatCount());
     }
+
+    public static int sumArray(String[][] array) throws MyArraySizeException, MyArrayDataException {
+        if (array.length != 4 || array[0].length != 4) {
+            throw new MyArraySizeException("Массив должен быть размером 4x4.");
+        }
+
+        int sum = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                try {
+                    sum += Integer.parseInt(array[i][j]);
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException("Неверные данные в ячейке [" + i + "][" + j + "]: " + array[i][j]);
+                }
+            }
+        }
+
+        return sum;
+    }
+
+
 }
 
-
-
-class Animal {
-    private static int totalCount = 0; // Счетчик всех животных
-
-    public Animal() {
-        totalCount++;
-    }
-
-    public static int getTotalCount() {
-        return totalCount;
-    }
-
-    public void run(int distance) {
-        throw new UnsupportedOperationException();
-    }
-
-    public void swim(int distance) {
-        throw new UnsupportedOperationException();
-    }
-}
-
-class Dog extends Animal {
-    private static int dogCount = 0; // Счетчик собак
-    private String name;
-
-    public Dog(String name) {
-        super();
-        this.name = name;
-        dogCount++;
-    }
-
-    @Override
-    public void run(int distance) {
-        if (distance <= 500 ) {
-            System.out.println(name + " пробежал " + distance + " м.");
-        } else {
-            System.out.println(name + " не может пробежать " + distance + " м.");
-        }
-    }
-
-
-
-    @Override
-    public void swim(int distance) {
-        if (distance <= 10) {
-            System.out.println(name + " проплыл " + distance + " м.");
-        } else {
-            System.out.println(name + " не может проплыть " + distance + " м.");
-        }
-    }
-
-    public static int getDogCount() {
-        return dogCount;
-    }
-}
-
-class Cat extends Animal {
-    private static int catCount = 0; // Счетчик котов
-    String name;
-    private boolean isFull; // Поле сытости
-
-    public Cat(String name) {
-        super();
-        this.name = name;
-        this.isFull = false; // Кот изначально голоден
-        catCount++;
-    }
-
-    @Override
-    public void run(int distance) {
-        if (!isFull) {
-            System.out.println(name + " голоден и не может бегать.");
-            return;
-        }
-        if (distance <= 200) {
-            System.out.println(name + " пробежал " + distance + " м.");
-        } else {
-            System.out.println(name + " не может пробежать " + distance + " м.");
-        }
-    }
-
-    @Override
-    public void swim(int distance) {
-        System.out.println(name + " не умеет плавать.");
-    }
-
-    public void eat(Bowl bowl, int amount) {
-
-        if (isFull) {
-            System.out.println(name + " уже сыт и не хочет есть.");
-            return;
-        }
-        if (bowl.getFoodAmount() >= amount) {
-            bowl.decreaseFood(amount);
-            isFull = true; // Кот становится сытым
-            System.out.println(name + " покушал " + amount + " еды.");
-        } else {
-            System.out.println(name + " не может покушать, недостаточно еды в миске.");
-        }
-    }
-
-    public boolean isFull() {
-        return isFull;
-    }
-
-    public static int getCatCount() {
-        return catCount;
-    }
-}
-
-
-
-class Bowl {
-    private int foodAmount;
-
-    public Bowl(int initialFood) {
-        if (initialFood < 0) {
-            this.foodAmount = 0; // Не допускаем отрицательного количества еды
-        } else {
-            this.foodAmount = initialFood;
-        }
-    }
-
-    public void addFood(int amount) {
-        if (amount > 0) {
-            foodAmount += amount;
-            System.out.println("В миску добавлено " + amount + " еды. Теперь в миске " + foodAmount + " еды.");
-        }
-    }
-
-    public void decreaseFood(int amount) {
-        if (amount <= foodAmount) {
-            foodAmount -= amount;
-        }
-    }
-
-    public int getFoodAmount() {
-        return foodAmount;
-    }
-}
 
